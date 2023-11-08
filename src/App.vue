581089d6a454e858fcddb66885bade068ea584c9
Alexander Kovalchuk <alexander.kovalchuk98@gmail.com>
2023-11-08 17:23:44 +0300

    graphql-request without subscription

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import MyAsyncComponent from './components/MyAsyncComponent.vue';
+
+// import { GraphQLClient, request, gql } from 'graphql-request'
+// import { ref } from 'vue';
+
+// const result = ref<any>(null)
+
+// const document = gql`
+//   {
+//     visitors {
+//           id
+//       }
+//   }
+// `
+
+// const endpoint = 'https://graphql.bubble.chat:8081/'
+
+// //  const result = await request('https://graphql.bubble.chat:8081/', document)
+
+// const client = new GraphQLClient(endpoint)
+// const result = await client.request(document)
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      Graphql Request
+    </div>
+  </header>
+  <div>
+    <Suspense>
+   <template #default>
+    <!-- {{ result }} -->
+    <MyAsyncComponent />
+   </template>
+   <template #fallback>
+     <span>Loading...</span>
+   </template>
+ </Suspense>
+  </div>
+
+  <main>
+    <TheWelcome />
+  </main>
+  
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/MyAsyncComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    {{ data }}
+</template>
+
+<script lang="ts">
+
+import { GraphQLClient, request, gql } from 'graphql-request'
+import { ref } from 'vue'
+
+export default {
+
+    name: 'MyAsyncComponent',
+    async setup() {
+        const document = gql`
+  {
+    visitors {
+          id
+      }
+  }
+`
+        const endpoint = 'https://graphql.bubble.chat:8081/'
+
+        const result = await request('https://graphql.bubble.chat:8081/', document)
+
+        // const client = new GraphQLClient(endpoint)
+        // const result = await client.request(document)
+
+        const data = ref<any>(null)
+
+        // await new Promise((r) => setTimeout(r, 2000))
+
+        try {
+            const res = await request('https://graphql.bubble.chat:8081/', document)
+            data.value = await res
+        } catch (e) {
+            console.error(e)
+        }
+        return { data }
+    }
+}
+
+</script>
